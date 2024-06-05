cc8e952c41b64148815c51b656aaa781746d7d21
Paulo Eduardo Neves <pauloneves@gmail.com>
2024-06-05 02:15:54 +0000

    copiei e colei comentário e regerei

--- comments.js
+++ comments.js
@@ -1,26 +1,53 @@
-// create web server
-// create a server
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var path = require('path');
+// Create web server that listens on port 3000
+// Respond to GET requests to /comments with a list of comments
+// Respond to POST requests to /comments by adding a new comment
+// Respond to DELETE requests to /comments by deleting the last comment
+// Respond to PUT requests to /comments by updating the last comment
+// Respond to all other requests with a 404 status code
 
-// create a server
-http.createServer(function(request, response) {
-    var pathname = url.parse(request.url).pathname;
-    var filepath = path.join(__dirname, 'comments', pathname);
-    fs.exists(filepath, function(exists) {
-        if (exists) {
-            fs.readFile(filepath, function(err, data) {
-                response.writeHead(200);
-                response.write(data);
-                response.end();
-            });
-        } else {
-            response.writeHead(404);
-            response.write('404 Not Found\n');
-            response.end();
-        }
+const http = require('http');
+const url = require('url');
+const qs = require('querystring');
+const port = 3000;
+
+const comments = [];
+
+const server = http.createServer((req, res) => {
+  const method = req.method;
+  const path = url.parse(req.url).pathname;
+  const query = qs.parse(url.parse(req.url).query);
+
+  if (method === 'GET' && path === '/comments') {
+    res.setHeader('Content-Type', 'application/json');
+    res.write(JSON.stringify(comments));
+    res.end();
+  } else if (method === 'POST' && path === '/comments') {
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk;
     });
-}).listen(8080);
-console.log('Server running at http://localhost:8080/');
+    req.on('end', () => {
+      comments.push(JSON.parse(body));
+      res.end();
+    });
+  } else if (method === 'DELETE' && path === '/comments') {
+    comments.pop();
+    res.end();
+  } else if (method === 'PUT' && path === '/comments') {
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk;
+    });
+    req.on('end', () => {
+      comments[comments.length - 1] = JSON.parse(body);
+      res.end();
+    });
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}...`);
+});
